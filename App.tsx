e4d49a00d5f36db5b264a29d5a650c7833413175
April Chi <achi@atlassian.com>
2021-07-17 00:37:17 +1000

    initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from 'react-dom';
+import * as React from 'react';
+import './app.css';
+
+const App = () => {
+    return (
+        <div className="landing-page">
+            <div className="title">
+                No
+            </div>         
+        </div>
+    )
+}
+
+export default App;
